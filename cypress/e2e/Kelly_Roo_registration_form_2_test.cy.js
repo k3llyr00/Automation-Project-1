93f25cf465373921fd44e564c2a1a74fcfc568c8
k3llyr00 <k3llyr00@gmail.com>
2024-05-07 15:29:05 +0300

    removed it.only

--- cypress/e2e/Kelly_Roo_registration_form_2_test.cy.js
+++ cypress/e2e/Kelly_Roo_registration_form_2_test.cy.js
@@ -48,7 +48,7 @@ describe('Section 1: Functional tests', () => {
         cy.get('.submit_button').should('be.enabled')
     })
     
-    it.only('User can submit form with all fields added', ()=>{
+    it('User can submit form with all fields added', ()=>{
         //Steps for filling in all fields
         cy.get('#username').type(username)
         cy.get('#email').type(email)
